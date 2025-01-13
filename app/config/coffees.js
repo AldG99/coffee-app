6cf6eda9947acc42aee107f53b39d0d0c621df06
AldG99 <a.garciaglz17@gmail.com>
2025-01-12 20:16:23 -0600

    Añadir lista de productos de café con datos e imágenes

---
+++ app/config/coffees.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    id: 1,
+    name: 'Cappuccino',
+    image: require('../../assets/coffees/nathan-dumlao-c2Y16tC3yO8-unsplash.jpg'),
+    price: '2.00',
+    description:
+      'El cappuccino es una bebida de café hecha con espresso y leche caliente. Tradicionalmente se prepara con leche vaporizada y se adorna con una pequeña cantidad de espuma.',
+    categoryId: 1,
+    rating: 4.5,
+    included: 'Con leche de avena',
+  },
+  {
+    id: 2,
+    name: 'Espresso',
+    image: require('../../assets/coffees/nathan-dumlao-ikU3J1nr52w-unsplash.jpg'),
+    price: '4.01',
+    description:
+      'El espresso es una bebida de café hecha con granos de café calientes que se tuestan y luego se preparan con agua caliente, lo que resulta en un café fuerte, con cuerpo y concentrado, típico de un cappuccino. Tradicionalmente se prepara con leche vaporizada y se adorna con una pequeña cantidad de espuma.',
+    categoryId: 1,
+    rating: 4.5,
+    included: 'Con leche de avena',
+  },
+  {
+    id: 3,
+    name: 'Latte',
+    image: require('../../assets/coffees/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg'),
+    price: '3.05',
+    description:
+      'El latte es una bebida de café hecha con espresso y leche vaporizada. Tradicionalmente se prepara con leche vaporizada y se adorna con una pequeña cantidad de espuma.',
+    categoryId: 4,
+    rating: 4.5,
+    included: 'Con leche de avena',
+  },
+  {
+    id: 4,
+    name: 'Flat White',
+    image: require('../../assets/coffees/robbie-down-LI8inyHnm_A-unsplash.jpg'),
+    price: '3.00',
+    description:
+      'El flat white es una bebida de café hecha con espresso y leche vaporizada. Tradicionalmente se prepara con leche vaporizada y se adorna con una pequeña cantidad de espuma.',
+    categoryId: 4,
+    rating: 4.5,
+    included: 'Con leche de avena',
+  },
+  {
+    id: 5,
+    name: 'Cappuccino',
+    image: require('../../assets/coffees/fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg'),
+    price: '4.00',
+    description:
+      'El cappuccino es una bebida de café hecha con espresso y leche caliente. Tradicionalmente se prepara con leche vaporizada y se adorna con una pequeña cantidad de espuma.',
+    categoryId: 2,
+    rating: 4.5,
+    included: 'Con leche de avena',
+  },
+  {
+    id: 6,
+    name: 'Espresso',
+    image: require('../../assets/coffees/taylor-franz-GJogrGZxKJE-unsplash.jpg'),
+    price: '5.00',
+    description:
+      'El espresso es una bebida de café hecha con granos de café calientes que se tuestan y luego se preparan con agua caliente, lo que resulta en un café fuerte, con cuerpo y concentrado, típico de un cappuccino. Tradicionalmente se prepara con leche vaporizada y se adorna con una pequeña cantidad de espuma.',
+    categoryId: 2,
+    rating: 4.5,
+    included: 'Con leche de avena',
+  },
+];
