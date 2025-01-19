7f0ad407912e69488a76568925c69fd485d94590
AldG99 <a.garciaglz17@gmail.com>
2025-01-18 18:13:27 -0600

    Importar CoffeeDetailsScreen y configuración

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import colors from './app/config/colors';
 import HomeScreens from './app/screens/HomeScreens';
+import CoffeeDetailsScreen from './app/screens/CoffeeDetailsScreen';
+import coffees from './app/config/coffees';
 
 const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: colors.dark }}>
-      <HomeScreens />
+      <CoffeeDetailsScreen coffee={coffees[2]} />
     </View>
   );
 };
