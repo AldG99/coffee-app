15b81cf047842f6701ff8bcc9b99d32b6e5e6b3f
AldG99 <a.garciaglz17@gmail.com>
2025-01-30 18:31:52 -0600

    Agregar nuevas variedades de café a la lista de opciones disponibles en la aplicación

--- app/config/categories.js
+++ app/config/categories.js
@@ -7,10 +7,45 @@ const categories = [
     id: 2,
     name: 'Espresso',
   },
-  { id: 3, name: 'Latte' },
+  {
+    id: 3,
+    name: 'Latte',
+  },
   {
     id: 4,
     name: 'Flat White',
+  },
+  {
+    id: 5,
+    name: 'Mocha',
+  },
+  {
+    id: 6,
+    name: 'Macchiato',
+  },
+  {
+    id: 7,
+    name: 'Americano',
+  },
+  {
+    id: 8,
+    name: 'Ristretto',
+  },
+  {
+    id: 9,
+    name: 'Nitro Coffee',
+  },
+  {
+    id: 10,
+    name: 'Affogato',
+  },
+  {
+    id: 11,
+    name: 'Irish Coffee',
+  },
+  {
+    id: 12,
+    name: 'Iced Coffee',
   },
 ];
 
